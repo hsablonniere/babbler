7b182e118f63506621c7d324625818add521c5a5
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2012-11-17 00:59:59 +0100

    Adding no-jQuery version

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 (function () {
-  var init,
+  var $ = function (selector) {
+        return document.querySelector(selector);
+      },
+      on = function (selector, events, callback) {
+        var i;
+
+        events = events.split(' ');
+
+        for (i = 0; i < events.length; i++) {
+          $(selector).addEventListener(events[i], callback, false);
+        }
+      },
+      init,
       itemTpl,
       items,
       getRandomId,
@@ -14,13 +26,15 @@
       countingInitDate;
 
   init = function () {
+    var itemId;
+
     // Load item template
-    itemTpl = $('.item-tpl').text();
-    $('.item-tpl').remove();
+    itemTpl = $('.item-tpl').innerHTML.trim();
+    $('.item-tpl').parentNode.removeChild($('.item-tpl'));
 
     // Initialize items
     items = JSON.parse(localStorage.getItem('babbler.items')) || {};
-    for (var itemId in items) {
+    for (itemId in items) {
       if (items.hasOwnProperty(itemId)) {
         addItem(items[itemId]);
       }
@@ -30,7 +44,7 @@
   };
 
   getRandomId = function () {
-    return Math.floor(Math.random() * 1e32).toString(36);
+    return 'item-' + Math.floor(Math.random() * 1e32).toString(36);
   };
 
   timeToMinSec = function (time) {
@@ -49,7 +63,8 @@
   };
 
   updateStorageAndDisplay = function () {
-    var totalCounter = 0;
+    var totalCounter = 0,
+        id;
 
     // Total counter
     if (Object.keys(items).length > 0) {
@@ -64,23 +79,23 @@
 
     // Total percent
     if (Object.keys(items).length > 1 && totalCounter > 1) {
-      $('.total-counter').html(timeToMinSec(totalCounter));
-      $('.total-percent').html('100%');
+      $('.total-counter').innerHTML = timeToMinSec(totalCounter);
+      $('.total-percent').innerHTML = '100%';
     } else {
-      $('.total-counter').html('');
-      $('.total-percent').html('');
+      $('.total-counter').innerHTML = '';
+      $('.total-percent').innerHTML = '';
     }
 
     // All the percents
-    for (var id in items) {
+    for (id in items) {
       if (Object.keys(items).length > 1) {
         if (totalCounter > 0) {
-          $('#' + id + ' .item-percent').html((items[id].counter * 100 / totalCounter).toFixed(0) + '%');
+          $('#' + id + ' .item-percent').innerHTML = (items[id].counter * 100 / totalCounter).toFixed(0) + '%';
         } else {
-          $('#' + id + ' .item-percent').html('');
+          $('#' + id + ' .item-percent').innerHTML = '';
         }
       } else {
-        $('#' + id + ' .item-percent').html('');
+        $('#' + id + ' .item-percent').innerHTML = '';
       }
     }
 
@@ -90,14 +105,18 @@
 
   // Add item to the DOM
   addItem = function (item) {
-    var tpl = itemTpl;
+    var tpl = itemTpl,
+        tplNode;
 
     tpl = tpl.replace('${id}', item.id);
     tpl = tpl.replace('${name}', item.name);
     tpl = tpl.replace('${counter}', timeToMinSec(item.counter));
     tpl = tpl.replace('${percent}', '0%');
 
-    $(tpl).appendTo('body');
+    tplNode = document.createElement('div');
+    tplNode.innerHTML = tpl;
+
+    $('body').appendChild(tplNode.children[0]);
   };
 
   addNewItem = function () {
@@ -109,16 +128,22 @@
   };
 
   // Help
-  $('.help').on('click', function () {
-    $('.help-message').toggleClass('hidden');
+  on('.help', 'click', function () {
+    $('.help-message').classList.toggle('hidden');
   });
 
   // Item add or rename
-  $('body').on('change keyup blur', '.item-name', function (ev) {
-    var $item = $(this).parents('.item'),
-        id = $item.attr('id'),
-        name = $(this).val(),
-        item = items[id];
+  on('body', 'change keyup blur', function (ev) {
+    var $item, id, name, item;
+
+    if (!ev.target.classList.contains('item-name')) {
+      return;
+    }
+
+    $item = ev.target.parentNode.parentNode;
+    id = $item.id;
+    name = ev.target.value;
+    item = items[id];
 
     if (name !== '' && id === 'new') {
       id = getRandomId();
@@ -128,10 +153,11 @@
         name: name,
         counter: 0
       };
-      updateStorageAndDisplay();
 
-      $item.attr('id', id);
+      $item.id = id;
       addNewItem();
+
+      updateStorageAndDisplay();
     }
     if (id !== 'new') {
       items[id].name = name;
@@ -140,54 +166,66 @@
   });
 
   // Item remove
-  $('body').on('click', '.item-remove', function (ev) {
-    var id = $(this).parents('.item').attr('id'),
-        item = items[id];
+  on('body', 'click', function (ev) {
+    var id, item;
+
+    if (!ev.target.classList.contains('item-remove')) {
+      return;
+    }
+
+    id = ev.target.parentNode.id;
+    item = items[id];
 
     if (countingIntervalId && item.id === countingItem.id) {
       countingIntervalId = clearInterval(countingIntervalId);
     }
 
     delete items[id];
-    $('#' + id).remove();
+    $('#' + id).parentNode.removeChild($('#' + id));
     updateStorageAndDisplay();
   });
 
   // Counter
-  $('body').on('click', '.item:not(#new)', function (ev) {
+  on('body', 'click', function (ev) {
     var $item,
         item;
 
     // Prevents bubble clicks on close and input
-    if ($(ev.target).is('.item, .item-counter, .item-percent')) {
-      $item = $(ev.currentTarget);
-      item = items[$item.attr('id')];
+    if (ev.target.classList.contains('item')) {
+      $item = ev.target;
+      item = items[$item.id];
+    } else if (ev.target.classList.contains('item-counter') ||
+        ev.target.classList.contains('item-percent')) {
+      $item = ev.target.parentNode;
+      item = items[$item.id];
+    } else {
+      return;
+    }
 
-      if (countingIntervalId == null) {
-        $item.addClass('active');
+    if (countingIntervalId == null) {
+      $item.classList.add('active');
+      $countingItem = $item;
+      countingItem = item;
+      countingInitDate = new Date().getTime();
+      countingInitValue = item.counter;
+
+      countingIntervalId = setInterval(function () {
+        var count = countingInitValue + new Date().getTime() - countingInitDate;
+        count = count - count % 1000;
+        $('#' + $countingItem.id + ' .item-counter').innerHTML = timeToMinSec(count);
+        countingItem.counter = count;
+        updateStorageAndDisplay();
+      }, 200);
+    } else {
+      $countingItem.classList.remove('active');
+      if (item.id === countingItem.id) {
+        countingIntervalId = clearInterval(countingIntervalId);
+      } else {
+        $item.classList.add('active');
         $countingItem = $item;
         countingItem = item;
         countingInitDate = new Date().getTime();
         countingInitValue = item.counter;
-
-        countingIntervalId = setInterval(function () {
-          var count = countingInitValue + new Date().getTime() - countingInitDate;
-          count = count - count % 1000;
-          $countingItem.find('.item-counter').html(timeToMinSec(count));
-          countingItem.counter = count;
-          updateStorageAndDisplay();
-        }, 200);
-      } else {
-        $countingItem.removeClass('active');
-        if (item.id === countingItem.id) {
-          countingIntervalId = clearInterval(countingIntervalId);
-        } else {
-          $item.addClass('active');
-          $countingItem = $item;
-          countingItem = item;
-          countingInitDate = new Date().getTime();
-          countingInitValue = item.counter;
-        }
       }
     }
   });
